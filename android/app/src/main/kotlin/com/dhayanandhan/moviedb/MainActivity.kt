35387cada5b059d60ac5a6b402e03091818f40a7
Dhayanandhan <dhayaec@gmail.com>
2019-09-14 12:59:44 +0530

    add android launcher and splash

--- android/app/src/main/kotlin/com/dhayanandhan/moviedb/MainActivity.kt
+++ android/app/src/main/kotlin/com/dhayanandhan/moviedb/MainActivity.kt
@@ -1,13 +1,29 @@
 package com.dhayanandhan.moviedb
 
+import android.os.Build
 import android.os.Bundle
 
 import io.flutter.app.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
+import android.view.WindowManager
+import android.view.ViewTreeObserver
 
-class MainActivity: FlutterActivity() {
+
+class MainActivity : FlutterActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    //make transparent status bar
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+      window.statusBarColor = 0x00000000
+    }
     GeneratedPluginRegistrant.registerWith(this)
+    //Remove full screen flag after load
+    val vto = flutterView.viewTreeObserver
+    vto.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
+      override fun onGlobalLayout() {
+        flutterView.viewTreeObserver.removeOnGlobalLayoutListener(this)
+        window.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+      }
+    })
   }
 }
